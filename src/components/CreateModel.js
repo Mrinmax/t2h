93e54c73b13b08676666fb872e7935141a110eab
Mrinal kanti sardar <64627885+Mrinmax@users.noreply.github.com>
2022-01-13 23:07:57 +0530

    rest of work

--- src/components/CreateModel.js
+++ src/components/CreateModel.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Button, Grid, Paper } from '@material-ui/core';
 
 function CreateModel() {
+    const paperStyle ={ padding:20, height:'85vh', width:'70%', margin:'40px auto'}
+
     return (
-        <div>
-            <h2>Create Model</h2>
-        </div>
+        <Container>
+            <Grid align="center">
+                <Paper elevation={20} style={paperStyle}>
+                    <h2>Please select a file </h2>
+                    <input type="file" />
+                    <Button type="submit"
+                    color="secondary"
+                    fullWidth
+                    variant="contained"
+                    >Upload File</Button>
+                </Paper>
+            </Grid>
+     </Container>
     )
 }
 
 export default CreateModel;
+
+const Container =styled.div`
+    background-color: #F8F9FB;
+    align-items: center;
+
+    Button {
+        width: 20%;
+    }
+`
+
